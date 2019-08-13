9db2bb959ba5fe24f02f6356aaf51ba5cdc0b82a
NG-12 <angelika.ales@gmail.com>
2019-08-13 16:19:10 +0300

    2 files modified, 1 added, 1 deleted

--- src/app/node-register/node-register.component.ts
+++ src/app/node-register/node-register.component.ts
@@ -1,21 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpService} from './http.service';
+//import {HttpService} from './http.service';
 import {Node} from './node';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-node-register',
   templateUrl: './node-register.component.html',
   styleUrls: ['./node-register.component.css'],
-  providers: [HttpService]
+  providers: [HttpClient]
 })
 
 export class NodeRegisterComponent {
 
+  postData: {"nodeGroupId": "UZ_Megafon"};
   groupName: string;
   response: any;
-  constructor(private httpService: HttpService){}
-  submit(){
+  //url: 'https://api.github.com/users/';
+  url= 'http://localhost:8080/node/test';
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })}
+
+  constructor(private http: HttpClient){}
+  submit(){
+    //this.http.get(this.url + this.groupName)
+    this.http.post(this.url, this.postData, this.httpOptions)
+    .subscribe
+    ((response)=>{this.response = response;
+    //console.log(this.response);
+    });
+    
   }
   
 }
